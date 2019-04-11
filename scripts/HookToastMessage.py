82476e2864561cbd2741fbe4a9c8ef095f2e589e
russell-shizhen <shizhen.sg@gmail.com>
2019-04-11 13:53:51 +0800

    backup

---
+++ scripts/HookToastMessage.py
@@ -0,0 +1,60 @@
+import frida, sys
+
+def on_message(message, data):
+    if message['type'] == 'send':
+        print("[*] {0}".format(message['payload']))
+    else:
+        print(message)
+
+jscode = """
+Java.perform(function () {
+    // Function to hook is defined here
+    var MainActivity = Java.use('com.arophix.decompileapk.MainActivity');
+
+    // Whenever button is clicked
+    MainActivity.onClick.implementation = function (v) {
+        // Show a message to know that the function got called
+        send('onClick');
+
+        // Call the original onClick handler
+        this.onClick(v);
+
+        // Log to the console that it's done, and we should have the flag!
+//        console.log('Done:' + JSON.stringify(this.cnt));
+    };
+});
+
+Java.perform(function () {
+    // Function to hook is defined here
+    var MainActivity = Java.use('com.arophix.decompileapk.MainActivity');
+
+    // Whenever button is clicked
+    MainActivity.isPhoneRooted.implementation = function (v) {
+        // Show a message to know that the function got called
+        send("Called - isPhoneRooted()");
+        return false;
+    };
+});
+"""
+
+hook_code = """
+    Interceptor.attach(Module.findExportByName("libc.so", "dlopen"), {
+        onEnter: function(args) {
+            console.log("args " + args);
+            console.log("dlopen called with: " + this.lib);
+        },
+        onLeave: function(retval) {
+            
+        }
+    });
+    """
+
+
+
+process = frida.get_usb_device().attach('com.arophix.decompileapk')
+# script = process.create_script(jscode)
+script = process.create_script(jscode)
+script.on('message', on_message)
+print('[*] Running CTF')
+script.load()
+sys.stdin.read()
--- scripts/hooknative.py
+++ scripts/hooknative.py
@@ -5,19 +5,33 @@ package_name = "com.arophix.decompileapk"
 
 def native_hooking_scripts():
     hook_code = """
-
 	var moduleName = "libnative-lib.so"; 
     var nativeFuncAddr = 0x00004d10; // $ nm --demangle --dynamic libnative-lib.so | grep "stringFromJNI"
 
-    Interceptor.attach(Module.findExportByName(null, "strcmp"), {
+    Interceptor.attach(Module.findExportByName(null, "dlopen"), {
         onEnter: function(args) {
             this.lib = Memory.readUtf8String(args[0]);
             console.log("dlopen called with: " + this.lib);
         },
         onLeave: function(retval) {
-            
+            if (this.lib.endsWith(moduleName)) {
+                console.log("ret: " + retval);
+                var baseAddr = Module.findBaseAddress(moduleName);
+                Interceptor.attach(baseAddr.add(nativeFuncAddr), {
+                    onEnter: function(args) {
+                        console.log("[-] hook invoked");
+                        console.log(JSON.stringify({
+                            a1: args[1].toInt32(),
+                            a2: Memory.readUtf8String(Memory.readPointer(args[2])),
+                            a3: Boolean(args[3])
+                        }, null, '\t'));
+                    }
+                });
+            }
         }
     });
+
+    
     """
     return hook_code
 
