1773e94247b2eb9519d13efe033b1b88ce733397
russell-shizhen <shizhen.sg@gmail.com>
2019-04-11 16:56:34 +0800

    clean up scripts

--- scripts/hooknative.py
+++ scripts/hooknative.py
@@ -3,7 +3,8 @@ import sys
 
 package_name = "com.arophix.decompileapk"
 
-def native_hooking_scripts():
+# Not working ...
+def native_hooking_scripts_1():
     hook_code = """
 	var moduleName = "libnative-lib.so"; 
     var nativeFuncAddr = 0x00004d10; // $ nm --demangle --dynamic libnative-lib.so | grep "stringFromJNI"
@@ -22,7 +23,16 @@ def native_hooking_scripts():
         }
     });
 
-    // Hook the JNI function by function name
+    """
+    return hook_code
+
+# Hook the JNI function by function name
+def native_hooking_scripts_2():
+    hook_code = """
+	var moduleName = "libnative-lib.so"; 
+    var nativeFuncAddr = 0x00004d10; // $ nm --demangle --dynamic libnative-lib.so | grep "stringFromJNI"
+
+    
     Interceptor.attach (Module.findExportByName (moduleName, "Java_com_arophix_decompileapk_MainActivity_stringFromJNI"), {
         onEnter: function (args) {
             // send (Memory.readUtf8String (args [1]));     
@@ -38,43 +48,49 @@ def native_hooking_scripts():
         }
     });
 
-    // see: https://www.frida.re/docs/functions/
-    Module.enumerateSymbolsSync(moduleName).forEach(function(symbol){
-        switch (symbol.name) {
-            case "Java_com_arophix_decompileapk_MainActivity_stringFromJNI":
-                /*
-                    $ c++filt "_ZN3art3JNI21RegisterNativeMethodsEP7_JNIEnvP7_jclassPK15JNINativeMethodib"
-                    art::JNI::RegisterNativeMethods(_JNIEnv*, _jclass*, JNINativeMethod const*, int, bool)
-                */
-                var RegisterNativeMethodsPtr = symbol.address;
-                console.log("RegisterNativeMethods is at " + RegisterNativeMethodsPtr);
-                Interceptor.attach(RegisterNativeMethodsPtr, {
-                    onEnter: function(args) {
-                        console.log("[-] hook invoked");
-                        console.log("RegisterNativeMethodsPtr:" + RegisterNativeMethodsPtr);
-                        console.log("Java_com_arophix_decompileapk_MainActivity_stringFromJNI called with address.");
-                    },
-                    onLeave: function (retval) {
-                        console.log("ret: " + retval);
-                        const dstAddr = Java.vm.getEnv().newStringUtf("Frida is hooking this displayed text from Native layer by address.");
-                        retval.replace(dstAddr);
-                    }
-                });
-                break;
-            case "_ZN3art3JNI9FindClassEP7_JNIEnvPKc": // art::JNI::FindClass
-                Interceptor.attach(symbol.address, {
-                    onEnter: function(args) {
-                        if (args[1] != null) {
-                            jclassAddress2NameMap[args[0]] = Memory.readCString(args[1]);
-                        }
-                    },
-                    onLeave: function (ignoredReturnValue) {}
-                });
-                break;
+    """
+    return hook_code
+
+# Hook the JNI function by function address -- Not yet working ...
+def native_hooking_scripts_3():
+    hook_code = """
+	var moduleName = "libnative-lib.so"; 
+    var nativeFuncAddr = '0x00004d10'; // $ nm --demangle --dynamic libnative-lib.so | grep "stringFromJNI"
+    
+    // see: https://www.frida.re/docs/functions/ 
+    function memAddress(memBase, idaBase, idaAddr) {
+        var offset = ptr(idaAddr).sub(idaBase);
+        var result = ptr(memBase).add(offset);
+        return result;
+    }
+
+    function idaAddress(memBase, idaBase, memAddr) {
+        var offset = ptr(memAddr).sub(memBase);
+        var result = ptr(idaBase).add(offset);
+        return result;
+    }
+    
+    const membase = Module.findBaseAddress(moduleName);
+    console.log("[+] membase: " + membase);
+    const addressOfStringFromJni = memAddress(membase, '0x0', '0x00004d10');
+    console.log("[+] addressOfStringFromJni: " + addressOfStringFromJni);
+    
+    Interceptor.attach(ptr(addressOfStringFromJni), {
+        onEnter: function (args) {
+            console.log("[++] addressOfStringFromJni: " + addressOfStringFromJni);
+        },
+        onLeave: function (ignoredReturnValue) {
+
         }
     });
 
-    // Frida official example https://www.frida.re/docs/javascript-api/, search "Interceptor"
+    """
+    return hook_code
+
+# Frida official example https://www.frida.re/docs/javascript-api/, search "Interceptor"
+def native_hooking_scripts_4():
+    hook_code = """
+
     Interceptor.attach(Module.getExportByName(null, 'read'), {
         onEnter: function (args) {
             console.log('Context information:');
@@ -103,10 +119,51 @@ def native_hooking_scripts():
     """
     return hook_code
 
+# Try enumerateSymbolsSync
+def native_hooking_scripts_5():
+    hook_code = """
+
+    var moduleName = 'libart.so'; 
+    var nativeFuncAddr = '0x00004d10'; // $ nm --demangle --dynamic libnative-lib.so | grep "stringFromJNI"
+    
+    Java.perform(function () {    
+        console.log("moduleName: " + moduleName);
+        // see: https://www.frida.re/docs/functions/
+        Module.enumerateSymbolsSync(moduleName).forEach(function(symbol){
+            console.log("symbol.name: " + symbol.name);
+            switch (symbol.name) {
+                case "Java_com_arophix_decompileapk_MainActivity_stringFromJNI":
+                    /*
+                        $ c++filt "_ZN3art3JNI21RegisterNativeMethodsEP7_JNIEnvP7_jclassPK15JNINativeMethodib"
+                        art::JNI::RegisterNativeMethods(_JNIEnv*, _jclass*, JNINativeMethod const*, int, bool)
+                    */
+                    var RegisterNativeMethodsPtr = symbol.address;
+                    console.log("RegisterNativeMethods is at " + RegisterNativeMethodsPtr);
+                    Interceptor.attach(RegisterNativeMethodsPtr, {
+                        onEnter: function(args) {
+                            console.log("[-] hook invoked");
+                            console.log("RegisterNativeMethodsPtr:" + RegisterNativeMethodsPtr);
+                            console.log("Java_com_arophix_decompileapk_MainActivity_stringFromJNI called with address.");
+                        },
+                        onLeave: function (retval) {
+                            console.log("ret: " + retval);
+                            const dstAddr = Java.vm.getEnv().newStringUtf("Frida is hooking this displayed text from Native layer by address.");
+                            retval.replace(dstAddr);
+                        }
+                    });
+                    break;
+            }
+        });
+    });
+
+    """
+    return hook_code
+
+##########################################################################################################
 device=frida.get_usb_device()
 #run package
 process = device.attach(package_name)
-script = process.create_script(native_hooking_scripts())
+script = process.create_script(native_hooking_scripts_5())
 
 # Here's some message handling..
 # [ It's a little bit more meaningful to read as output :-D
