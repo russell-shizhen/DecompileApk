ee27e711fbd3739da03e595d222a94d1b7ad0b29
russell-shizhen <shizhen.sg@gmail.com>
2019-04-11 14:31:06 +0800

    add hooking test for 'libc.so', 'read'

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,18 @@
 #include <jni.h>
 #include <string>
+#include <unistd.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_arophix_decompileapk_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
+    
+    int length = strlen("Arophix Frida Hooking Example."); // length = 30
+    
+    
+    char buf[10] = {0,1,2,3,45,5,6,7,8,9};
+    int r = read(0, buf, 10);
+    
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
--- scripts/hooknative.py
+++ scripts/hooknative.py
@@ -8,6 +8,20 @@ def native_hooking_scripts():
 	var moduleName = "libnative-lib.so"; 
     var nativeFuncAddr = 0x00004d10; // $ nm --demangle --dynamic libnative-lib.so | grep "stringFromJNI"
 
+    Interceptor.attach(Module.findExportByName(null, "dlopen"), {
+        onEnter: function(args) {
+            this.lib = Memory.readUtf8String(args[0]);
+            console.log("dlopen called with: " + this.lib);
+        },
+        onLeave: function(retval) {
+            if (this.lib.endsWith(moduleName)) {
+                console.log("ret: " + retval);
+                var baseAddr = Module.findBaseAddress(moduleName);
+                
+            }
+        }
+    });
+
     Interceptor.attach (Module.findExportByName ( moduleName, "Java_com_arophix_decompileapk_MainActivity_stringFromJNI"), {
         onEnter: function (args) {
             // send (Memory.readUtf8String (args [1]));     
@@ -18,10 +32,25 @@ def native_hooking_scripts():
         onLeave: function (retval) {
             //print("[!] " +"onLeave called...")
             //retval.replace("Frida hooking ongoing ...");
+            console.log("ret: " + retval);
             }
         });
 
-    
+    Interceptor.attach(Module.getExportByName('libc.so', 'read'), {
+        onEnter: function (args) {
+            this.fileDescriptor = args[0].toInt32();
+            console.log("libc.so read() onEnter called.");
+            console.log("this.fileDescriptor: " + this.fileDescriptor);
+        },
+        onLeave: function (retval) {
+            console.log("libc.so read() onLeave called, retval:  " + retval);
+            if (retval.toInt32() > 0) {
+                /* do something with this.fileDescriptor */
+                
+            }
+        }
+    });
+
     """
     return hook_code
 
